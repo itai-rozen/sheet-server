39171902f7ddc44d6dbad983a25c9d378f0dff49
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-26 13:44:39 +0300

    fixed insert bug. deleted comments & irrelevant funcs

--- src/index.js
+++ src/index.js
@@ -27,7 +27,8 @@ const router = express.Router()
 
 
 const sqlQuery = query => {
-  const [rows] = db.query(query, (err, results) => {
+  console.log('hi sqlQuery ', query)
+  const rows = db.query(query, (err, results) => {
     if (err) console.log('Error: ', err)
     return results
   })
@@ -45,36 +46,16 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log('/')
-  // const query = ` ;`
   const { body } = req
   console.log('body : ', body.toString())
-  const query = getSqlInsertQuery('all_rows', JSON.parse(body.toString()))
+  const query = getSqlInsertQuery('invites', JSON.parse(body.toString()))
+  console.log('query: ',query)
   sqlQuery(query)
   res.send('success')
 })
 
-router.post('/invalid', (req, res) => {
-  const { body } = req
-  const query = getSqlInsertQuery('invalid_rows', body)
-  sqlQuery(query)
-  res.end()
-})
 
-router.post('/create-table', async (req, res) => {
-  console.log('/create-table')
-  const { body } = req
-  const query1 = "DROP TABLE IF EXISTS all_rows,invalid_rows,valid_rows;"
-  const query2 = `CREATE TABLE all_rows(${body.map(key => key !== 'rowNum' ? `${key} text` : `${key} int PRIMARY KEY NOT NULL`).join()} );`
-  const query3 = `CREATE TABLE invalid_rows(rowNum INT,problem VARCHAR(255),value TEXT,FOREIGN KEY (rowNum) REFERENCES all_rows(rowNum) );`
-  try {
-    sqlQuery(query1)
-    sqlQuery(query2)
-    sqlQuery(query3)
-  } catch (err) {
-    console.log(err)
-  }
-  res.end()
-})
+
 
 app.use('/.netlify/functions/index', router)
 
