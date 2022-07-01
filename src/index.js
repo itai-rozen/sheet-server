92a72832f142550bfadab718461f91edbd37d55a
itai rozen <itai.rozen@mail.huji.ac.il>
2022-07-01 09:31:10 +0300

    added root endpoint

--- src/index.js
+++ src/index.js
@@ -28,7 +28,8 @@ const sqlQuery = query => {
 }
 
 getSqlInsertQuery = (tableName, obj) => `INSERT INTO ${tableName} (${Object.keys(obj).join()}) VALUES (${Object.values(obj).map(val => `'${val}'`).join()});`
-app.get('/test', (req,res) => res.send('testing!'))
+
+app.get('/', (req,res) => res.end())
 app.get('/invites', (req, res) => {
   console.log('entered app.get')
   db.query("SELECT * FROM invites limit 1;", (err,results) => {
